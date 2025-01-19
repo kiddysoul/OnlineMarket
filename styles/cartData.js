91e4586699ccf81fdb5ad1d835420d1b3f8c1555
kiddysoul <timekb555@gmail.com>
2025-01-19 23:33:28 +0500

    cart start

---
+++ styles/cartData.js
@@ -0,0 +1,19 @@
+const cartData = () => {
+    const cart = document.querySelector('.js-cart');
+
+    const updateCartItemCount = () => {
+
+    };
+    updateCartItemCount();
+
+    const addProductToCart() => {
+        productsList.addEventListener('click',{event} => {
+            
+        })
+    };
+    addProductToCart();
+};
+
+export {
+    cartData
+};
